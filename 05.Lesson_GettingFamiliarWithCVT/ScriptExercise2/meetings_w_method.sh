5e8c0b44536e1520fa173cab90cd42421c5c21a2
YufeiLi <yufeil.li@lukka.tech>
2022-02-27 11:51:44 -0500

    06

--- 05.Lesson_GettingFamiliarWithCVT/ScriptExercise2/meetings_w_method.sh
+++ 05.Lesson_GettingFamiliarWithCVT/ScriptExercise2/meetings_w_method.sh
@@ -3,6 +3,6 @@ solc-select use 0.8.7
 
 certoraRun MeetingSchedulerFixed.sol:MeetingScheduler --verify MeetingScheduler:meetings.spec \
 --send_only \
---rule startOnTime \
+--rule startBeforeEnd \
 --method "startMeeting(uint256)" \
 --msg "$1"
